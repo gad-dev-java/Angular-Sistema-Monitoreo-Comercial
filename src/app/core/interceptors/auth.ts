6174097aadbd793a8e9f16fa1944699317d9fe97
gad-dev-java <josuealva920@gmail.com>
2026-03-13 15:15:10 -0500

    update company component

--- src/app/core/interceptors/auth.ts
+++ src/app/core/interceptors/auth.ts
@@ -8,9 +8,18 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  // 1. Identificamos si es una petición de login
+  const isLoginRequest = req.url.includes('/api/auth/login');
+
+  // 2. Si es login, limpiamos el storage para no enviar basura al backend
+  if (isLoginRequest) {
+    localStorage.clear();
+  }
+
   const token = authService.getToken();
 
-  const authReq = token
+  // 3. Solo adjuntamos el token si existe y NO es una petición de login
+  const authReq = (token && !isLoginRequest)
     ? req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
@@ -20,8 +29,6 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(authReq).pipe(
     catchError((error: HttpErrorResponse) => {
-      // Solo redirige en rutas protegidas (no en /login)
-      const isLoginRequest = req.url.includes('/api/auth/login');
       if (error.status === 401 && !isLoginRequest) {
         authService.logout();
         router.navigate(['/login']);
